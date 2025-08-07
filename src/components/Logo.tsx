a048396c7a612c3f6019fc3d75fe6fec6cb4e910
Suki <tues1205@naver.com>
2025-08-07 23:10:20 +0900

    ✨ : Logo 컴포넌트

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import LOGO from "../assets/images/main-logo.svg";
+
+interface LogoProps {
+  width?: string;
+  height?: string;
+}
+
+const Logo: React.FC<LogoProps> = ({ width = "8.8vw", height = "2.34vw" }) => {
+  return <img src={LOGO} alt="Logo" style={{ width, height }} />;
+};
+
+export default Logo;
