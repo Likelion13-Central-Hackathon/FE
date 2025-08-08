b904b09b5cca4efb10156d6f12223935247054a1
mgbae02 <mgbae02@naver.com>
2025-08-09 01:45:02 +0900

    💄배경

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import background from '../assets/images/error-background.svg';
 
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+  <div>
+  <img src={background} alt="background"/>
+  </div>
+  );
 };
 
 export default NotFoundPage;
