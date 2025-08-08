94f25741b5a9b5dbe8b56db5aa666027506e1c55
Suki <tues1205@naver.com>
2025-08-08 15:55:24 +0900

    ✨ : TitleContainer 컴포넌트

---
+++ src/components/TitleContainer.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import s from "./styles/TitleContainer.module.scss";
+import { ImageSizeProps } from "../types/common";
+
+interface TitleContainerProps extends ImageSizeProps {
+  imgSrc: string;
+  text: string;
+}
+
+const TitleContainer: React.FC<TitleContainerProps> = ({
+  imgSrc,
+  text,
+  width = "27.55vw",
+  height = "5.92vw",
+}) => {
+  return (
+    <div className={s.titleContainer}>
+      <img
+        src={imgSrc}
+        alt="TitleContainer-logo"
+        className={s.titleImage}
+        style={{ width, height }}
+      />
+      <p className={s.titleText}>{text}</p>
+    </div>
+  );
+};
+
+export default TitleContainer;
