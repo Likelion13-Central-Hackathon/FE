48f93876695993b18b989c42d7b129e38110f339
Suki <tues1205@naver.com>
2025-08-09 00:59:00 +0900

    💄 : 메인페이지 Title 애니메이션

--- src/components/TitleContainer.tsx
+++ src/components/TitleContainer.tsx
@@ -4,6 +4,7 @@ import { SizeTextProps } from "../types/common";
 
 interface TitleContainerProps extends SizeTextProps {
   imgSrc: string;
+  motionType?: "init" | "exit" | "enter"; // 모션 타입
 }
 
 const TitleContainer: React.FC<TitleContainerProps> = ({
@@ -11,13 +12,14 @@ const TitleContainer: React.FC<TitleContainerProps> = ({
   text,
   width = "27.55vw",
   height = "5.92vw",
+  motionType = "init",
 }) => {
   return (
     <div className={s.titleContainer}>
       <img
         src={imgSrc}
         alt="TitleContainer-logo"
-        className={s.titleImage}
+        className={`${s.titleImage} ${s[motionType]}`}
         style={{ width, height }}
       />
       <p className={s.titleText}>{text}</p>
