30665eb0e31335d038422a5a69f32a30345cc54d
Suki <tues1205@naver.com>
2025-08-09 18:16:24 +0900

    ♻️ : MainPage에 IntroLayout 적용

--- src/components/IntroLayout.tsx
+++ src/components/IntroLayout.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import s from "../pages/styles/MainPage.module.scss";
+import { SizeProps } from "../types/common";
 import TitleContainer from "./TitleContainer";
 import FloatingMotion from "./FloatingMotion";
 
-interface IntroProps {
+interface IntroProps extends SizeProps {
   title: string;
   text: string;
   children?: React.ReactNode; // 하위 컴포넌트
 }
 
-const IntroLayout: React.FC<IntroProps> = ({ title, text, children }) => {
+const IntroLayout: React.FC<IntroProps> = ({
+  title,
+  text,
+  width = "27.55vw",
+  height = "13.18vw",
+  children,
+}) => {
   return (
     <div className={s.mainPageWrapper}>
       <div className={s.titleChildren}>
         <div className={s.titleContainer}>
           <TitleContainer
-            width="27.55vw"
-            height="13.18vw"
+            width={width}
+            height={height}
             imgSrc={title}
             text={text}
           />
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import s from "./styles/MainPage.module.scss";
-import TitleContainer from "../components/TitleContainer";
 import TITLE from "../assets/images/first-logo.svg";
-import FloatingMotion from "../components/FloatingMotion";
 import BasicButton from "../components/BasicButton";
+import IntroLayout from "../components/IntroLayout";
 
 const MainPage = () => {
   const navigate = useNavigate();
@@ -14,25 +13,22 @@ const MainPage = () => {
   };
 
   return (
-    <div className={s.mainPageWrapper}>
-      <div className={s.titleContainer}>
-        <TitleContainer
-          imgSrc={TITLE}
-          text={`청년의 시작, 확신으로 바꾸다.\n청년창업의 실질적인 어려움을 함께 해결합니다.`}
+    <IntroLayout
+      title={TITLE}
+      text={`청년의 시작, 확신으로 바꾸다.\n청년창업의 실질적인 어려움을 함께 해결합니다.`}
+      height="5.92vw"
+    >
+      <div className={s.buttonContainer}>
+        <BasicButton
+          text="분석할각?"
+          onClick={() => handleNavigate("/form-intro")}
         />
-        <div className={s.buttonContainer}>
-          <BasicButton
-            text="분석할각?"
-            onClick={() => handleNavigate("/form-intro")}
-          />
-          <BasicButton
-            text="선정될각?"
-            onClick={() => handleNavigate("/document")}
-          />
-        </div>
+        <BasicButton
+          text="선정될각?"
+          onClick={() => handleNavigate("/document-intro")}
+        />
       </div>
-      <FloatingMotion />
-    </div>
+    </IntroLayout>
   );
 };
 
