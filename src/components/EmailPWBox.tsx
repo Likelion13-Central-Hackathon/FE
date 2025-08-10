cc6224783386831ba37a700f07c793b4661d23b2
Suki <tues1205@naver.com>
2025-08-10 19:29:02 +0900

    💄 : EmailPWBox 디자인 변경사항 수정

--- src/components/EmailPWBox.tsx
+++ src/components/EmailPWBox.tsx
@@ -18,35 +18,38 @@ const EmailPWBox: React.FC<EmailPWBoxProps> = ({
 }) => {
   return (
     <div className={s.emailBox}>
-      <label className={s.label} htmlFor="email-input">
-        E-mail
-      </label>
-      <input
-        id="email-input"
-        className={s.input}
-        type="email"
-        lang="en"
-        inputMode="email"
-        placeholder="이메일을 입력하세요"
-        value={email}
-        onChange={(e) => onChangeEmail(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && onSubmit?.()}
-        autoComplete="email"
-      />
-
-      <label className={s.label} htmlFor="pw-input">
-        Password
-      </label>
-      <input
-        id="pw-input"
-        className={s.input}
-        type="password"
-        placeholder="비밀번호를 입력하세요"
-        value={password}
-        onChange={(e) => onChangePassword(e.target.value)}
-        onKeyDown={(e) => e.key === "Enter" && onSubmit?.()}
-        autoComplete="current-password"
-      />
+      <section>
+        <label className={s.label} htmlFor="email-input">
+          E-mail
+        </label>
+        <input
+          id="email-input"
+          className={s.input}
+          type="email"
+          lang="en"
+          inputMode="email"
+          placeholder="이메일을 입력하세요"
+          value={email}
+          onChange={(e) => onChangeEmail(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && onSubmit?.()}
+          autoComplete="email"
+        />
+      </section>
+      <section>
+        <label className={s.label} htmlFor="pw-input">
+          Password
+        </label>
+        <input
+          id="pw-input"
+          className={s.input}
+          type="password"
+          placeholder="비밀번호를 입력하세요"
+          value={password}
+          onChange={(e) => onChangePassword(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && onSubmit?.()}
+          autoComplete="current-password"
+        />
+      </section>
     </div>
   );
 };
