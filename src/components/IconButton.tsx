3f65d152f8a973f74b76768e699343fdc93e4d45
Suki <tues1205@naver.com>
2025-08-10 22:26:11 +0900

    ✨ : IconButton 컴포넌트

---
+++ src/components/IconButton.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+interface IconButtonProps {
+  imgSrc: string;
+  text: string;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ imgSrc, text }) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <img src={imgSrc} />
+      <p style={{ fontSize: "0.73vw", fontWeight: "500", color: "#757575" }}>
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default IconButton;
