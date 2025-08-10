3f49750dc07a0179028c8160fba7ae4b5c9c0d00
Suki <tues1205@naver.com>
2025-08-11 01:08:02 +0900

    💄 : ReportOutBox column 가능하도록

--- src/pages/reportPage/components/ReportOutBox.tsx
+++ src/pages/reportPage/components/ReportOutBox.tsx
@@ -4,15 +4,17 @@ import { SizeProps } from "../../../types/common";
 
 interface OutBoxProps extends SizeProps {
   children?: React.ReactNode; // 하위 컴포넌트
+  className?: string;
 }
 
 const ReportOutBox: React.FC<OutBoxProps> = ({
   width = "63.65vw",
   height = "13.07vw",
   children,
+  className,
 }) => {
   return (
-    <div className={s.outbox} style={{ width, height }}>
+    <div className={`${s.outbox} ${className || ""}`} style={{ width, height }}>
       {children}
     </div>
   );
