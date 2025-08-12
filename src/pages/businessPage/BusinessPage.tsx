534d4c24efa3502328b9365d65cef5caf57a6174
Suki <tues1205@naver.com>
2025-08-12 18:31:38 +0900

    ✨ : MapBox 컴포넌트

---
+++ src/pages/businessPage/BusinessPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MapBox from "./components/MapBox";
+
+const BusinessPage = () => {
+  return (
+    <div>
+      <MapBox />
+    </div>
+  );
+};
+
+export default BusinessPage;
---
+++ src/pages/businessPage/components/RegionButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import s from "./Business.module.scss";
+
+interface RegionButtonProps {
+  region: string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+const RegionButton: React.FC<RegionButtonProps> = ({ region, style }) => {
+  return (
+    <div className={s.regionButton} style={style}>
+      {region}
+    </div>
+  );
+};
+
+export default RegionButton;
