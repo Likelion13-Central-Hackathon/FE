5d5c15624daba251eae788d707bfd603fb80401d
Suki <tues1205@naver.com>
2025-08-14 02:01:42 +0900

    ♻️ : RankItem, ResultItem 타입 재정의

--- src/types/business.ts
+++ src/types/business.ts
@@ -5,6 +5,33 @@ export type Region = (typeof regions)[number];
 export type RegionName = Region["name"];
 
 // ResultItem, RankItem
-export type RankImageProps = {
-  rankImg: string; // 이미지 경로
+type RankItemCommon = {
+  title: string;
+  startDate: string;
+  endDate: string;
+  suitability: number;
+  agency: string;
+  supportArea: string;
+};
+
+type RankItemSimple = RankItemCommon & {
+  recommendedId: number;
+};
+
+type RankItemDetail = RankItemCommon & {
+  link: string;
+  region: string;
+  stage: string;
+  target: string;
+  contact: string;
+  applyMethod: string;
+  supportDetails: string;
+  requiredDocuments: string;
+  applyProcedureAndEvaluation: string;
+  reason: string;
+};
+
+export type RankItemProps = {
+  rankImg: string;
+  item: RankItemSimple | RankItemDetail; // 유니온
 };
