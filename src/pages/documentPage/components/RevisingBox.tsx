97fc3ab276b49c5be98b84babef20fec1ee4e732
Suki <tues1205@naver.com>
2025-08-15 03:27:21 +0900

    🎨 : RevisingBox 구조 수정

--- src/pages/documentPage/components/RevisingBox.tsx
+++ src/pages/documentPage/components/RevisingBox.tsx
@@ -48,57 +48,69 @@ const RevisingBox: React.FC<RevisingTitle> = ({ title, explanation }) => {
             초기차업패키지 / 예비창업패키지 / 창업도약패키지 사업계획서 길잡이.
           </p>
         </div>
-        <ReportOutBox width="61.56vw" height="35.47vw" className={b.column}>
-          {/* 문항 정보 */}
-          <section className={s.itemInfo}>
-            <span>{title}</span>
-            <p>{explanation}</p>
-          </section>
+        <ReportOutBox width="61.56vw" height="35.47vw">
+          <div
+            style={{ display: "flex", flexDirection: "column", gap: "2.60vw" }}
+          >
+            {/* 문항 정보 */}
+            <section className={s.itemInfo}>
+              <span>{title}</span>
+              <p>{explanation}</p>
+            </section>
 
-          <section style={{ display: "flex", gap: "1.35vw" }}>
-            {/* 사용자 답변 입력 */}
-            <ReportOutBox width="24.32vw" height="22.92vw" className={b.column}>
-              <p className={s.guideText}>답변 입력하기</p>
-              <ReportInBox width="22.24vw" height="18.02vw">
-                <textarea
-                  placeholder="답변을 입력하세요."
-                  className={s.textarea}
+            <section style={{ display: "flex", gap: "1.35vw" }}>
+              {/* 사용자 답변 입력 */}
+              <ReportOutBox
+                width="24.32vw"
+                height="22.92vw"
+                className={b.column}
+              >
+                <p className={s.guideText}>답변 입력하기</p>
+                <ReportInBox width="22.24vw" height="18.02vw">
+                  <textarea
+                    placeholder="답변을 입력하세요."
+                    className={s.textarea}
+                  />
+                </ReportInBox>
+              </ReportOutBox>
+
+              {/* 첨삭받기 버튼 */}
+              <div className={s.returnButton}>
+                <img
+                  src={RETURN}
+                  alt="return-button"
+                  style={{ width: "3.75vw" }}
+                  className={rotating ? s.rotate : ""}
                 />
-              </ReportInBox>
-            </ReportOutBox>
-
-            {/* 첨삭받기 버튼 */}
-            <div className={s.returnButton}>
-              <img
-                src={RETURN}
-                alt="return-button"
-                style={{ width: "3.75vw" }}
-                className={rotating ? s.rotate : ""}
-              />
-              <BasicButton
-                width="5.31vw"
-                height="1.67vw"
-                text="첨삭받기"
-                onClick={handleRevising}
-              />
-            </div>
-
-            {/* AI 첨삭 답변 + 복사 버튼 */}
-            <ReportOutBox width="24.32vw" height="22.92vw" className={b.column}>
-              <div className={s.aiTitle}>
-                <p className={s.guideText}>AI 첨삭 답변</p>
                 <BasicButton
-                  width="4.27vw"
+                  width="5.31vw"
                   height="1.67vw"
-                  text="복사"
-                  onClick={handleCopy}
+                  text="첨삭받기"
+                  onClick={handleRevising}
                 />
               </div>
-              <ReportInBox width="22.24vw" height="18.02vw">
-                <div className={s.aiResponse}>{aiAnswer}</div>
-              </ReportInBox>
-            </ReportOutBox>
-          </section>
+
+              {/* AI 첨삭 답변 + 복사 버튼 */}
+              <ReportOutBox
+                width="24.32vw"
+                height="22.92vw"
+                className={b.column}
+              >
+                <div className={s.aiTitle}>
+                  <p className={s.guideText}>AI 첨삭 답변</p>
+                  <BasicButton
+                    width="4.27vw"
+                    height="1.67vw"
+                    text="복사"
+                    onClick={handleCopy}
+                  />
+                </div>
+                <ReportInBox width="22.24vw" height="18.02vw">
+                  <div className={s.aiResponse}>{aiAnswer}</div>
+                </ReportInBox>
+              </ReportOutBox>
+            </section>
+          </div>
         </ReportOutBox>
       </div>
     </GradientBox>
