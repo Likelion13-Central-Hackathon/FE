1fcd0c4945d4cc6b7abbd1dca9ff7faf543641fe
Suki <tues1205@naver.com>
2025-08-15 03:34:05 +0900

    🚧 : DocumentPage 각도기 빼고

--- src/pages/documentPage/DocumentPage.tsx
+++ src/pages/documentPage/DocumentPage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import s from "../styles/DocumentPage.module.scss";
+import DocumentItem from "./components/DocumentItem";
+import { revisingTitles } from "../../data/RevisingTitleData";
 
 const DocumentPage = () => {
-  return <div>DocumentPage</div>;
+  return (
+    <div style={{ display: "flex", gap: "5.73vw" }}>
+      <div style={{ width: "12.45vw", backgroundColor: "yellow" }}>
+        각도기 영역
+      </div>
+      <div className={s.container}>
+        {revisingTitles.map((item, index) => (
+          <div key={index} className={s.section}>
+            <DocumentItem title={item.title} explanation={item.explanation} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DocumentPage;
