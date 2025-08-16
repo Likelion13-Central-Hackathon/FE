e7ec12f60a191f70f80a9c899753f5b288f597c4
Suki <tues1205@naver.com>
2025-08-17 01:38:01 +0900

    ✨ : Loading 컴포넌트

---
+++ src/components/Loading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import s from "./styles/Loading.module.scss";
+import SvgLogo from "./SvgLogo";
+import FloatingMotion from "./FloatingMotion";
+import PLUS3D from "../assets/images/plus-3d.svg";
+
+const Loading = () => {
+  return (
+    <div className={s.loadingContainer}>
+      <FloatingMotion mirrored={true} />
+      <div className={s.loadingLogo}>
+        <div className={s.logoImg}>
+          <SvgLogo />
+          <p>정확한 분석을 위해 1분 정도 소요될 수 있습니다.</p>
+        </div>
+      </div>
+      <img src={PLUS3D} alt="plus-3d" className={s.plus3D} />
+    </div>
+  );
+};
+
+export default Loading;
