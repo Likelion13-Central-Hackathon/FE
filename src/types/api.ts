228358c3f0797f5cb4a92195f762d97915d03ff4
Suki <tues1205@naver.com>
2025-08-20 15:22:51 +0900

    ♻️ : ApiEnvelope null값 가능한 타입 추가

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,8 +1,17 @@
-export type ApiEnvelope<T> = {
+type ApiBase = {
   isSuccess: boolean;
   code: string;
   httpStatus: number;
   message: string;
-  data: T;
   timeStamp: string;
 };
+
+// 항상 데이터가 있는 응답
+export interface ApiEnvelope<T> extends ApiBase {
+  data: T;
+}
+
+// 데이터가 없을 수도 있는 응답
+export interface ApiEnvelopeNullable<T> extends ApiBase {
+  data: T | null;
+}
