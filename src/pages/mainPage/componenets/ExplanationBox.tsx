efa7ffea6ac96189f0c327eac2692c6676127318
Suki <tues1205@naver.com>
2025-08-23 12:31:58 +0900

    🎨 : ExplanationBox 이동

--- src/pages/mainPage/componenets/ExplanationBox.tsx
+++ src/pages/mainPage/componenets/ExplanationBox.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import s from "./styles/ExplanationBox.module.scss";
-import { ExplationBox } from "../types/common";
+import s from "../styles/ExplanationBox.module.scss";
+import { ExplationBoxProps } from "../../../types/common";
 
 // MainPage에 있는 분석할각, 선정될각 설명
-const ExplanationBox: React.FC<ExplationBox> = ({
+const ExplanationBox: React.FC<ExplationBoxProps> = ({
   imgSrc,
   width,
   title,
